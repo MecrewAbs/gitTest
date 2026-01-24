66a47abce7ebe56d3eb41ab58ff68b2d3eecedf2
Andrey <bakrovee@gmail.com>
2026-01-24 18:38:09 +0300

    work by OS2

--- main.go
+++ main.go
@@ -1,5 +1,59 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"gitTest/files"
+	"gitTest/storage"
+)
 
+func main() {
+	bins := []storage.Bin{
+		{ID: "1", Value: "first"},
+		{ID: "2", Value: "second"},
+	}
+
+	// Сохранение в файл
+	err := storage.SaveToJSON(bins, "bins.json")
+	if err != nil {
+		fmt.Println("Ошибка сохранения:", err)
+		return
+	}
+
+	// Загрузка из файла
+	loadedBins, err := storage.LoadFromJSON("bins.json")
+	if err != nil {
+		fmt.Println("Ошибка загрузки:", err)
+		return
+	}
+
+	fmt.Printf("Загружено: %+v\n", loadedBins)
+
+	filename := "data.json"
+
+	exists, err := files.IsFileExists(filename)
+	if err != nil {
+		fmt.Println("Ошибка проверки файла:", err)
+		return
+	}
+	if !exists {
+		fmt.Println("Файл не существует")
+		return
+	}
+
+	// Проверка расширения
+	if !files.IsJSONFile(filename) {
+		fmt.Println("Файл не является JSON")
+		return
+	}
+
+	// Чтение файла
+	data, err := files.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Ошибка чтения файла:", err)
+		return
+	}
+
+	fmt.Println(string(data))
 }
+
+//2dwdwd
--- storage/storage.go
+++ storage/storage.go
@@ -1,1 +1,38 @@
 package storage
+
+import (
+	"encoding/json"
+	"os"
+)
+
+type Bin struct {
+	ID    string `json:"id"`
+	Value string `json:"value"`
+}
+
+func SaveToJSON(bins []Bin, filename string) error {
+	data, err := json.Marshal(bins)
+	if err != nil {
+		return err
+	}
+	err = os.WriteFile(filename, data, 0644)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+func LoadFromJSON(fileName string) ([]Bin, error) {
+
+	data, err := os.ReadFile(fileName)
+	if err != nil {
+		return nil, err
+	}
+	var bins []Bin
+	err = json.Unmarshal(data, &bins)
+	if err != nil {
+		return nil, err
+	}
+	return bins, nil
+}
+
+//dwdwd
